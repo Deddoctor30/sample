5f8e7768ab6ba710759608233777d8f19ec09030
Deddoctor30 <alinsbomb@yandex.ru>
2022-02-16 16:48:24 +0300

    16.02.2022 at work

--- js/test.js
+++ js/test.js
@@ -1,5 +1,24 @@
-import 'jquery';
+function duplicateEncode(word){
+   let b = word.toLowerCase();
+   let a = b.split('');
+   for (let i = 0; i < word.length; i++) {
+      console.log(a[i]);
+      if (a[i] === b){
 
-const elem = $('menu__item');
+      }
+   }
+}
 
-console.log(elem);
+
+const i = 'Success';
+
+duplicateEncode(i);
+
+// console.log(duplicateEncode(i));
+
+// console.log(text23.toLowerCase().includes(searchText.toLowerCase()));
+
+// if (word[i]) {
+
+// };
+
